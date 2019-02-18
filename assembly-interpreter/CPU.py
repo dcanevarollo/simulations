459da8e63de791437d62019968ecd13fe7cc07c3
Douglas Canevarollo <douglascanevarollo@gmail.com>
2019-02-18 09:22:56 -0300

    Add files via upload

--- assembly-interpreter/CPU.py
+++ assembly-interpreter/CPU.py
@@ -1,19 +1,23 @@
 from modules.ControlUnit import *
 
-file_name = input("Assembly's file name:\n> ")
-
-# Adds the extension '.txt' if the name read don't has one.
-if ".txt" not in file_name:
-    file_name += ".txt"
-
-entry = int(input("\nInitial value:\n> "))
-
 try:
+    file_name = input("Assembly's file name:\n> ")
+
+    # Adds the extension '.txt' if the name read don't has one.
+    if ".txt" not in file_name:
+        file_name += ".txt"
+
+    entry = int(input("\nInitial value:\n> "))
+
     assembly = open("docs/" + file_name, "r")
     store(assembly, entry)  # Stores the instructions and the initial value.
     decode()  # Main function that interprets the assembly's code.
     assembly.close()
 except FileNotFoundError:
-    print("\nError! Could not open the file '%s'." % file_name)
+    print("\nError! Could not open the file.")
+    input("\nPress enter to exit the prompt.\n> ")
+    exit(1)
+except ValueError:
+    print("\nError! The initial value must be an integer.")
     input("\nPress enter to exit the prompt.\n> ")
     exit(1)
